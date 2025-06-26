93fa78a83c35ee9e48e303bea72cce5a13b24302
SahiruSathmira <binewmindinu1@gmail.com>
2025-06-26 17:50:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.VAJIRA-MD=b0oVzbpa#E2t6D9JXEpNBCJrwqHu3tQLLZrPsrP5MllHKik-0X_8 === undefined ? '' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94719199757' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
